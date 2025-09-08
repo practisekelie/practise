0acfaa55d0304b6d952f8e1202d118b71dc015cd
aman <amanaj402@gmail.com>
2025-09-08 11:32:11 +0530

    resource code

---
+++ main.tf
@@ -0,0 +1,10 @@
+resource "azurerm_resource_group" "amn-rg" {
+  name     = "amanrg_name"
+  location = "central India"
+}
+
+
+resource "azurerm_resource_group" "aman-rg1" {
+  name     = "amanrg1_name1"
+  location = "central India"
+}
---
+++ provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.43.0"
+    }
+  }
+}
+
+provider "azurerm" {
+ features {}
+    
+    subscription_id = "76095b6e-0585-402e-8e43-281835aebfd1"
+ }
